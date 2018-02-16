cc1121c5f40df31c2dc4bf2b37ca431cafd0b232
JacksonHuang <gamegamelife@gmail.com>
2018-02-16 21:37:39 +0800

    dynamic set sleep time

--- app/src/main/java/com/example/jacksonhuang/backgroundsample/Common.java
+++ app/src/main/java/com/example/jacksonhuang/backgroundsample/Common.java
@@ -1,6 +1,7 @@
 package com.example.jacksonhuang.backgroundsample;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 /**
  * Created by gameg on 2018/2/14.
@@ -10,12 +11,23 @@ public class Common {
     public static final int SLEEP_TIME = 2000 ;  // ms
 
     public static String getResult(Context context){
-        MyLog.d("");
+        MyLog.d(" getResult start");
         try{
-            Thread.sleep(SLEEP_TIME);
+            int sleep_time = getSleepTime(context);
+            Thread.sleep(sleep_time*1000);
         }catch (InterruptedException e){
             // do nothing
         }
+        MyLog.d(" getResult finish");
         return context.getString(R.string.calulate_finish);
+    }
+
+    public static int getSleepTime(Context context){
+        SharedPreferences preferences = context.getSharedPreferences("pref" , Context.MODE_PRIVATE);
+        return preferences.getInt("sleep_time" , 3);
+    }
+    public static void setSleepTime(Context context, int sleep_time){
+        SharedPreferences preferences = context.getSharedPreferences("pref" , Context.MODE_PRIVATE);
+        preferences.edit().putInt("sleep_time" , sleep_time).apply();
     }
 }
--- app/src/main/java/com/example/jacksonhuang/backgroundsample/MainActivity.java
+++ app/src/main/java/com/example/jacksonhuang/backgroundsample/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.jacksonhuang.backgroundsample;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -39,6 +44,21 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(getBaseContext() , RxJavaActivity.class));
+            }
+        });
+
+        // edit sleep time
+        final EditText editText = findViewById(R.id.edit_sleep);
+        int sleep_time = Common.getSleepTime(this);
+        editText.setText(String.valueOf(sleep_time));
+
+        // set sleep time
+        findViewById(R.id.btn_set_sleep_time).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int sleep_time = Integer.valueOf(editText.getText().toString());
+                Common.setSleepTime(getBaseContext(), sleep_time);
+                Toast.makeText(getBaseContext() , getString(R.string.set_sleep_time), Toast.LENGTH_LONG).show();
             }
         });
     }
